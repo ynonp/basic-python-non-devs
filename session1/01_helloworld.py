b2bffcde460c1430db4d3074896175edf7a1e562
ynonp <ynonperek@gmail.com>
2020-12-23 11:00:00 +0200

    first examples

---
+++ session1/01_helloworld.py
@@ -0,0 +1,29 @@
+# A First Python Program
+
+####################
+# 1. A # is a comment line
+#
+
+"""
+2. 3 quotes is also a comment
+
+3. Python will read your file and run the program line-by-line
+
+4. My first python function.... print()
+"""
+# PyCharm - that's my IDE
+
+# String Literal
+print("Hello World")
+
+# Exception - ZeroDivisionError:
+print(f"2 + 5 is ... {2 + 5}")
+print('Bye bye')
+
+# A break until 10:50
+
+
+
+
+
+
---
+++ session1/02_input_output.py
@@ -0,0 +1,14 @@
+# Python input
+
+print("Who are you?")
+name = input()
+
+print(f"Happy to see you {name}")
+
+# int / float
+print("And how old are you?")
+age = int(input())
+
+print(f"Wow next year you'll be {age + 1}")
+
+
